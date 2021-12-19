2a39d8de1e3a69b75e30dcd87d805028bd44cdc5
Marcio <marcioabcd11@gmail.com>
2021-12-19 19:10:40 -0300

    Ready to deploy

--- src/service/baseUrl.js
+++ src/service/baseUrl.js
@@ -1,5 +1,5 @@
 import process from 'process';
 
-const BASE_URL = process.env.NODE_ENV === 'production' ? 'teste' : 'http://localhost:4000';
+const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://repoprovas-marcio.herokuapp.com' : 'http://localhost:4000';
 
 export default BASE_URL;
